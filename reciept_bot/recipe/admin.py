98654245fca353ffd807bd235a16cfb063773989
Weffy61 <weffy@mail.ru>
2023-09-21 04:26:46 +0300

    Update admin

--- reciept_bot/recipe/admin.py
+++ reciept_bot/recipe/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
+from .models import Recipe, Subscription, Categories, Client, LikeDislike, UserCategoryView
 
-# Register your models here.
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('title', 'get_category')
+
+    def get_category(self, obj):
+        return obj.category.category
+
+    get_category.short_description = 'Category'
+
+
+@admin.register(Subscription)
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = ('client', 'subscription_is_active')
+
+
+@admin.register(Categories)
+class CategoriesAdmin(admin.ModelAdmin):
+    list_display = ('category_id', 'category')
+
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('tg_id', 'username', 'name')
+
+
+@admin.register(LikeDislike)
+class LikeDislikeAdmin(admin.ModelAdmin):
+    list_display = ('client', 'recipe', 'action')
+
+
+@admin.register(UserCategoryView)
+class UserCategoryViewAdmin(admin.ModelAdmin):
+    list_display = ('user', 'category', 'views_left', 'date')
+
+
+
