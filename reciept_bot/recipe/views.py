746abcbc4623f8d4cdbef887c5031eb3f6a54ced
Weffy61 <weffy@mail.ru>
2023-09-21 04:27:16 +0300

    Add views

--- reciept_bot/recipe/views.py
+++ reciept_bot/recipe/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from recipe.models import Recipe
 
-# Create your views here.
+
+def serialize_recipe(recipe):
+    return {
+        "title": recipe.title,
+        'cooking_time': recipe.cooking_time,
+        'description': recipe.description,
+        'ingredients': recipe.ingredients,
+        'price': recipe.price,
+        'vegan_recipe': recipe.vegan_recipe,
+        'category': recipe.category,
+        'image': recipe.image
+        # "slug": recipe.slug,
+    }
+
+
+def index(request):
+    all_recipes = Recipe.objects.all()[:3]
+
+    context = {
+        'recipes': [serialize_recipe(recipe) for recipe in all_recipes]
+    }
+    return render(request, 'index.html', context)
