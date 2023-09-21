7684e9c6cbd359d6b1ab9afa34bd243bbef44b8a
Weffy61 <weffy@mail.ru>
2023-09-21 04:25:27 +0300

    Update settings, add ulrs

--- reciept_bot/reciept_bot/settings.py
+++ reciept_bot/reciept_bot/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 from environs import Env
 import dj_database_url
@@ -5,9 +6,9 @@ import dj_database_url
 env = Env()
 env.read_env()
 
+TELEGRAM_TOKEN = env.str('TELEGRAM_TOKEN')
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -45,10 +46,13 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'reciept_bot.urls'
 
+TEMPLATE_DIR = os.path.join(BASE_DIR, '../templates')
+
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -94,7 +98,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
@@ -102,11 +106,21 @@ USE_I18N = True
 
 USE_TZ = True
 
-
+# STATICFILES_FINDERS = [
+#     'django.contrib.staticfiles.finders.FileSystemFinder',
+#     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+# ]
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"), )
+
+MEDIA_ROOT = env.str("MEDIA_ROOT", os.path.join(BASE_DIR, 'media'))
+# MEDIA_ROOT = f'{BASE_DIR}/media'
+STATIC_ROOT = env.str("STATIC_ROOT")
+MEDIA_URL = env.str('MEDIA_URL', '/media/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
