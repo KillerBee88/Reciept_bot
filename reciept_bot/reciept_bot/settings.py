d3e88fb89ff5ab261a49b3027eb20e845fc60feb
Weffy61 <weffy@mail.ru>
2023-09-24 20:18:45 +0300

    Update settings

--- reciept_bot/reciept_bot/settings.py
+++ reciept_bot/reciept_bot/settings.py
@@ -7,22 +7,15 @@ env = Env()
 env.read_env()
 
 TELEGRAM_TOKEN = env.str('TELEGRAM_TOKEN')
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env.str('SECRET_KEY', 'REPLACE_ME')
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', True)
 
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', [])
 
-
-# Application definition
 
 INSTALLED_APPS = [
     'recipe',
@@ -68,16 +61,10 @@ TEMPLATES = [
 WSGI_APPLICATION = 'reciept_bot.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
 DATABASES = {
     'default': dj_database_url.parse(env('DATABASE_URL'))
 }
 
-
-# Password validation
-# https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -95,9 +82,6 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-# Internationalization
-# https://docs.djangoproject.com/en/4.2/topics/i18n/
-
 LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
@@ -106,23 +90,12 @@ USE_I18N = True
 
 USE_TZ = True
 
-# STATICFILES_FINDERS = [
-#     'django.contrib.staticfiles.finders.FileSystemFinder',
-#     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-# ]
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
-
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"), )
 
 MEDIA_ROOT = env.str("MEDIA_ROOT", os.path.join(BASE_DIR, 'media'))
-# MEDIA_ROOT = f'{BASE_DIR}/media'
 STATIC_ROOT = env.str("STATIC_ROOT")
 MEDIA_URL = env.str('MEDIA_URL', '/media/')
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
